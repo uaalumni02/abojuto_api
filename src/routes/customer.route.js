cac1ed7ff50021fb49b01cd2c3f9df63c138baa0
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-08-19 11:04:31 -0400

    add check-auth

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import customerController from "../controllers/customer";
+import checkAuth from "../middleware/check-auth";
 
 const router = express.Router();
 
-router.route("/:id").get(customerController.getCustomerById);
+router.route("/:id").get(checkAuth, customerController.getCustomerById);
 
 router.route("/").post(customerController.addCustomerData);
 router.route("/").get(customerController.getAllCustomers);
