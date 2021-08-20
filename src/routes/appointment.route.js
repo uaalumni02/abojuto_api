c4488088f0fb9f7c602390f1f4459fa3c3a5feb1
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-08-20 11:35:52 -0400

    add auth checker

--- src/routes/appointment.route.js
+++ src/routes/appointment.route.js
@@ -1,15 +1,15 @@
 import express from "express";
 import appointmentController from "../controllers/appointments";
+import checkAuth from "../middleware/check-auth";
 
 const router = express.Router();
 
 router.route("/:id").get(appointmentController.getAppointmentById);
 
-router.route("/").post(appointmentController.addAppointmentData);
+router.route("/").post(checkAuth, appointmentController.addAppointmentData);
 
 router.route("/").get(appointmentController.getAllAppointments);
 
 router.route("/:id").get(appointmentController.getAppointmentByUserorCustomer);
-
 
 export default router;
